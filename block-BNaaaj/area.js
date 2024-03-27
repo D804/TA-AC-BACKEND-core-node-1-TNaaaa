0bbe6d078f038438b3199dd32d3c43e494211d69
Deepak kushwaha <61049435+D804@users.noreply.github.com>
2024-03-27 10:40:28 +0530

    node js assignment 1

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,14 @@
+let squareArea = (area) => {
+  return area * area;
+};
+let circleArea = (radius) => {
+  return Math.PI * radius * radius;
+};
+let rectangleArea = (lemgth, width) => {
+  return length * width;
+};
+module.exports = {
+  squareArea: squareArea,
+  circleArea: circleArea,
+  rectangleArea: rectangleArea,
+};
